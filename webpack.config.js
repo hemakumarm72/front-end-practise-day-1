d4b70dc4f8a9d36296887eed107e445f9c287952
Hemakumar <hemakumarm72@gmail.com>
2022-12-16 18:39:55 +0530

    optimation performance speed

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const CompressionPlugin = require('compression-webpack-plugin'); // gzip
+const BrotliPlugin = require('brotli-webpack-plugin'); // brotli
+
+module.exports = {
+    plugins: [
+        new CompressionPlugin({
+            // gzip plugin
+            filename: '[path].gz[query]',
+            algorithm: 'gzip',
+            test: /\.(js|css|html|svg)$/,
+            threshold: 8192,
+            minRatio: 0.8,
+        }),
+        new BrotliPlugin({
+            // brotli plugin
+            asset: '[path].br[query]',
+            test: /\.(js|css|html|svg)$/,
+            threshold: 10240,
+            minRatio: 0.8,
+        }),
+        new BundleAnalyzerPlugin(),
+    ],
+    mode: 'development',
+    resolve: {
+        extensions: ['.js', '.jsx', '.css'],
+    },
+};
