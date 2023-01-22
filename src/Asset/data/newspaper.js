cf9739806741b806a9b15245c2a9419266767fff
Hemakumar <hemakumarm72@gmail.com>
2023-01-22 21:30:27 +0530

    dummy data and image assest

---
+++ src/Asset/data/newspaper.js
@@ -0,0 +1,22 @@
+export const newspaper = [
+    {
+        name: 'Home',
+        link: '/',
+    },
+    {
+        name: 'New',
+        link: '/new',
+    },
+    {
+        name: 'Popular',
+        link: '/popular',
+    },
+    {
+        name: 'Treding',
+        link: '/treding',
+    },
+    {
+        name: 'Categories',
+        link: '/categories',
+    },
+];
--- src/Pages/news_paper/main_page.jsx
+++ src/Pages/news_paper/main_page.jsx
@@ -1,13 +1,23 @@
+import { newspaper } from 'Asset/data/newspaper';
+import { Container } from 'Components';
 import React from 'react';
-
+import { NavLink } from 'react-router-dom';
 // import { Container } from './styles';
 
 function Newspaper() {
     return (
-        <>
-            <p>d</p>
-            <p>d</p>
-        </>
+        <Container>
+            <header>
+                <img src="" alt="" />
+                <nav>
+                    {newspaper.map((d) => (
+                        <NavLink activeClassName="active" exact to={d.link}>
+                            {d.name}
+                        </NavLink>
+                    ))}
+                </nav>
+            </header>
+        </Container>
     );
 }
 
